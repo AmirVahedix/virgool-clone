bc0de0e06602e8e69e108b8ca7cdfdcbbdf34303
Amir Vahedi <amirvahedix@gmail.com>
2023-01-18 22:47:54 +0330

    Redirect all routings to vue router

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('{url?}', function ($url = null) {
-    return view('welcome');
-})->where('url', '.*');
+Route::fallback(function () {
+    return view('home');
+});
