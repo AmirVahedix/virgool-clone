c89d4a9248db203319eb0e5b28c4177c83fc83d1
Amir Vahedi <amirvahedix@gmail.com>
2023-01-19 21:40:08 +0330

    Created search component

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -1,12 +1,16 @@
+<script setup>
+import Search from "./Search.vue";
+</script>
+
 <template>
     <v-container>
-        <v-row  class="px-3 sm:px-0" no-gutters>
+        <v-row class="px-3 sm:px-0" no-gutters>
             <v-row align="center" class="py-6" no-gutters>
                 <img src="/assets/images/icon.svg" class="h-12" alt="ویرگول"/>
                 <a href="" class="text-blue-500 hover:text-blue-600 mr-2">ویرگول چیست؟</a>
                 <v-spacer></v-spacer>
-                <v-btn icon="mdi-magnify" variant="text" color="grey"></v-btn>
-                <v-btn :to="{ name: 'login' }" variant="text" class="ml-3 mr-1">
+                <Search />
+                <v-btn :to="{ name: 'login' }" variant="text" class="mx-1">
                     ورود
                 </v-btn>
                 <v-btn :to="{ name: 'register' }" variant="flat" color="blue-darken-3">
@@ -16,13 +20,3 @@
         </v-row>
     </v-container>
 </template>
-
-<script>
-export default {
-    name: "Header"
-}
-</script>
-
-<style scoped>
-
-</style>
---
+++ resources/js/Components/Search.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {ref} from "vue";
+
+const search = ref(false);
+</script>
+
+<template>
+    <v-menu
+        v-model="search"
+        :close-on-content-click="false"
+        min-width="100%"
+        transition="slide-y-transition"
+    >
+        <template v-slot:activator="{ props }">
+            <v-btn
+                @click="search = true"
+                icon="mdi-magnify"
+                variant="text"
+                color="grey"
+            ></v-btn>
+        </template>
+        <v-container>
+            <div class="bg-white py-3 px-4">
+                <v-row align="center">
+                    <v-text-field
+                        variant="plain"
+                        placeholder="در بین مقالات، نویسندگان و… جستجو کنید"
+                    ></v-text-field>
+                    <v-btn
+                        icon="mdi-close"
+                        variant="plain"
+                        color="grey lighten-3"
+                        @click="search = false"
+                    ></v-btn>
+                </v-row>
+            </div>
+        </v-container>
+    </v-menu>
+</template>
